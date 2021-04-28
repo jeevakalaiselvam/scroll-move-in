a63dccc5c9f44100ed4deba9e8679769b40174ca
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-29 04:05:12 +0530

    comments added

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+//Reference to all cards in DOM
 const cards = document.querySelectorAll(".card");
 
-window.addEventListener("scroll", bringInBoxes);
+//Check if user scrolls the window and trigger bringing in cards
+window.addEventListener("scroll", bringInCards);
 
-bringInBoxes();
+bringInCards();
 
-function bringInBoxes() {
+/**
+ * This function checks if the user has scrolled the window to a certain threshold and checks if any card is present in that range, If yes, It uses CSS3 animation to bring in them into view
+ */
+function bringInCards() {
     const threshold = (window.innerHeight / 5) * 4;
 
+    //Apply CSS3 classes to cards withing accepting conditions
     cards.forEach((card) => {
         if (card.getBoundingClientRect().top < threshold) {
             card.classList.add("active");
